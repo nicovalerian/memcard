4bddc83b001c7a091edc627c4be69d11f2c73d06
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-08-19 17:20:36 +0700

    refactor CSS and add pokemon text exceptions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,34 @@
 import '../styles/Card.css'
 import PropTypes from 'prop-types';
-import "../styles/Card.css";
+
+const exceptions = {
+  'great-tusk': 'Great Tusk',
+  'scream-tail': 'Scream Tail',
+  'brute-bonnet': 'Brute Bonnet',
+  'flutter-mane': 'Flutter Mane',
+  'slither-wing': 'Slither Wing',
+  'sandy-shocks': 'Sandy Shocks',
+  'roaring-moon': 'Roaring Moon',
+  'walking-wake': 'Walking Wake',
+  'gouging-fire': 'Gouging Fire',
+  'raging-bolt': 'Raging Bolt',
+  'iron-treads': 'Iron Treads',
+  'iron-bundle': 'Iron Bundle',
+  'iron-hands': 'Iron Hands',
+  'iron-jugulis': 'Iron Jugulis',
+  'iron-moth': 'Iron Moth',
+  'iron-thorns': 'Iron Thorns',
+  'iron-valiant': 'Iron Valiant',
+  'iron-leaves': 'Iron Leaves',
+  'iron-boulder': 'Iron Boulder',
+  'iron-crown': 'Iron Crown'
+};
 
 function filterName(name) {
+  if (exceptions[name]) {
+    return exceptions[name];
+  }
+  
   const [firstPart] = name.split('-');
   return firstPart.charAt(0).toUpperCase() + firstPart.slice(1);
 }
--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -12,63 +12,67 @@ function Level() {
   const [pokemonData, setPokemonData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchPokemonData = async () => {
-      try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1302');
-        const data = await response.json();
-    
-        // Determine the number of Pokémon to fetch based on difficulty
-        const limit = difficulty === "easy" ? 16 : 25;
-    
-        // Randomly select a subset of Pokémon
-        const selectedPokemon = [];
-        const usedIndices = new Set();
-        while (selectedPokemon.length < limit) {
-          const randomIndex = Math.floor(Math.random() * data.results.length);
-          if (!usedIndices.has(randomIndex)) {
-            usedIndices.add(randomIndex);
-            selectedPokemon.push(data.results[randomIndex]);
-          }
+  const fetchPokemonData = async () => {
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1302');
+      const data = await response.json();
+  
+      // Determine the number of Pokémon to fetch based on difficulty
+      const limit = difficulty === "easy" ? 16 : 25;
+  
+      // Randomly select a subset of Pokémon
+      const selectedPokemon = [];
+      const usedIndices = new Set();
+      while (selectedPokemon.length < limit) {
+        const randomIndex = Math.floor(Math.random() * data.results.length);
+        if (!usedIndices.has(randomIndex)) {
+          usedIndices.add(randomIndex);
+          selectedPokemon.push(data.results[randomIndex]);
         }
-    
-        // Fetch details for the selected Pokémon
-        let pokemonDetails = await Promise.all(
-          selectedPokemon.map(async (pokemon) => {
-            const pokemonResponse = await fetch(pokemon.url);
-            return pokemonResponse.json();
-          })
-        );
-    
-        // Filter out Pokémon without sprites.front_default
-        let filteredPokemonDetails = pokemonDetails.filter(pokemon => pokemon.sprites.front_default);
-    
-        // Continue fetching additional Pokémon until we have the required number
-        while (filteredPokemonDetails.length < limit) {
-          const randomIndex = Math.floor(Math.random() * data.results.length);
-          if (!usedIndices.has(randomIndex)) {
-            usedIndices.add(randomIndex);
-            const pokemon = data.results[randomIndex];
-            const pokemonResponse = await fetch(pokemon.url);
-            const pokemonDetail = await pokemonResponse.json();
-            if (pokemonDetail.sprites.front_default) {
-              filteredPokemonDetails.push(pokemonDetail);
-            }
-          }
-        }
-    
-        setPokemonData(filteredPokemonDetails);
-      } catch (error) {
-        console.error("Failed to fetch Pokémon data:", error);
-      } finally {
-        setLoading(false);
       }
-    };
-    
-    fetchPokemonData();
+  
+      // Fetch details for the selected Pokémon
+      let pokemonDetails = await Promise.all(
+        selectedPokemon.map(async (pokemon) => {
+          const pokemonResponse = await fetch(pokemon.url);
+          return pokemonResponse.json();
+        })
+      );
+  
+      // Filter out Pokémon without sprites.front_default
+      let filteredPokemonDetails = pokemonDetails.filter(pokemon => pokemon.sprites.front_default);
+  
+      // Continue fetching additional Pokémon until we have the required number
+      while (filteredPokemonDetails.length < limit) {
+        const randomIndex = Math.floor(Math.random() * data.results.length);
+        if (!usedIndices.has(randomIndex)) {
+          usedIndices.add(randomIndex);
+          const pokemon = data.results[randomIndex];
+          const pokemonResponse = await fetch(pokemon.url);
+          const pokemonDetail = await pokemonResponse.json();
+          if (pokemonDetail.sprites.front_default) {
+            filteredPokemonDetails.push(pokemonDetail);
+          }
+        }
+      }
+  
+      setPokemonData(filteredPokemonDetails);
+    } catch (error) {
+      console.error("Failed to fetch Pokémon data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchPokemonData();
-  }, [difficulty]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleRefresh = () => {
+    fetchPokemonData();
+    setCurrentScore(0);
+  };
 
   if (loading) {
     return (
@@ -87,6 +91,7 @@ function Level() {
               <button className="back-button">Back</button>
             </Link>
             <Scoreboard currentScore={currentScore} highScore={highScore} />
+            <button className="refresh-button" onClick={handleRefresh}>Refresh</button>
           </div>
           <section className="game-board">
             <CardGrid
