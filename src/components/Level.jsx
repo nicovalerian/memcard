55d378bbd33dd0d8d89f10871559b86d9b9d317d
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-08-19 14:06:37 +0700

    Polish the level layout design

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,24 +1,26 @@
-import { Link } from "react-router-dom"
-import CardGrid from './CardGrid'
-import Scoreboard from './Scoreboard'
-import "../styles/Level.css"
+import { Link } from "react-router-dom";
+import CardGrid from "./CardGrid";
+import Scoreboard from "./Scoreboard";
+import "../styles/Level.css";
 
 function Level() {
   return (
     <>
-        <div className="mainscreen-wallpaper">
+      <div className="mainscreen-wallpaper">
+        <div className="game-wrapper">
+          <div className="top-board">
             <Link to="/">
-                <button className="back-button">Back</button>
+              <button className="back-button">Back</button>
             </Link>
-            <div className="game-wrapper">
-              <Scoreboard />
-              <section className="game-board">
-                <CardGrid />
-              </section>
-            </div>
+            <Scoreboard />
+          </div>
+          <section className="game-board">
+            <CardGrid />
+          </section>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Level
+export default Level;
